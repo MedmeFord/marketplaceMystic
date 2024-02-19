3d80ce60c53aa045a6d04c40a61af577bd8fd5e9
MedmeFord <89875845827@mail.ru>
2024-02-19 15:17:28 +0300

    new Backend

---
+++ src/main/java/ru/mystic/marketplacemystic/MarketplaceMysticApplication.java
@@ -0,0 +1,13 @@
+package ru.mystic.marketplacemystic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceMysticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceMysticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mystic/marketplacemystic/MarketplaceMysticApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mystic.marketplacemystic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceMysticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
